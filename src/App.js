c9b9014831f46af10854a52e29b91e291f5a3bff
Lucas Larsson <lucas.lucasman@gmail.com>
2021-06-23 20:03:19 +0200

    bleh

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let x = {name:"default"};
+  fetch("api/nas/storage/",{mode:'no-cors',method:'GET'}).then(response=>response.json()).then(data=>x=data)
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +14,11 @@ function App() {
           <a class="App-link" href="mailto:lucas.e.larsson@telia.com"> 
             Kontakt
           </a>
+          <div className="nas-container">
+            <p>Status: <span>Online</span></p>
+            <p>Pool0: {x.name} <span>Online</span>. Healthy: <span>True</span></p>
+            <p>Disk space left: <span>1.5TB</span></p>
+          </div>
       </header>
     </div>
   );
